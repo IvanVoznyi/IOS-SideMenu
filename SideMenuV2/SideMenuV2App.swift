dbc72af9e4e13e401d2c9c59607a23d4a4391ba4
Ivan Voznyi <ivan.voznyi92@gmail.com>
2024-01-30 17:56:50 +0200

    Initial Commit

---
+++ SideMenuV2/SideMenuV2App.swift
@@ -0,0 +1,17 @@
+//
+//  SideMenuV2App.swift
+//  SideMenuV2
+//
+//  Created by Ivan Voznyi on 30.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct SideMenuV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
